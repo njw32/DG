1fe96b080dbfe643ed767adb38a15a5cb6008ea6
unknown <njw32@students.calvin.edu>
2020-12-03 13:14:47 -0500

    the bag basic disc card

--- discGolf/src/app/components/disc-card/disc-card.component.ts
+++ discGolf/src/app/components/disc-card/disc-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-disc-card',
@@ -6,9 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./disc-card.component.scss'],
 })
 export class DiscCardComponent implements OnInit {
+  @Input() discName: string;
+  @Input() discManufacturer: string;
+  @Input() discPlastic: string;
+  @Input() discType: string;
+  @Input() discWeight: number;
+  @Input() discSpeed: number;
+  @Input() discGlide: number;
+  @Input() discTurn: number;
+  @Input() discFade: number;
 
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() { }
 
 }
