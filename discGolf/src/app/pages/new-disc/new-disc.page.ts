b7a62d5e302af7762516e2f99ca64dc109feb6f8
unknown <njw32@students.calvin.edu>
2020-12-03 14:17:53 -0500

    add a new disc to db

--- discGolf/src/app/pages/new-disc/new-disc.page.ts
+++ discGolf/src/app/pages/new-disc/new-disc.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-new-disc',
@@ -7,9 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewDiscPage implements OnInit {
 
-  constructor() { }
+  discName: string;
+  discManufacturer: string;
+  discPlastic: string;
+  discType: string;
+  discSpeed: number;
+  discGlide: number;
+  discTurn: number;
+  discFade: number;
+  discWeight: number;
+
+  constructor(private db: AngularFirestore, private router: Router) { }
 
   ngOnInit() {
   }
 
+  submitDisc() {
+    this.db.collection("discs").add({
+      Name: this.discName,
+      Manufacturer: this.discManufacturer,
+      Plastic: this.discPlastic,
+      Type: this.discType,
+      Speed: this.discSpeed,
+      Glide: this.discGlide,
+      Turn: this.discTurn,
+      Fade: this.discFade,
+      Weight: this.discWeight,
+    });
+    this.router.navigateByUrl('/the-bag');
+  }
 }
