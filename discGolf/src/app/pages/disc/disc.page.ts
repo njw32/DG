f39e359d882e7de05175d90dc9d278fe5715c49b
unknown <njw32@students.calvin.edu>
2020-12-04 00:18:22 -0500

    searched colleciton console.log correct doc

--- discGolf/src/app/pages/disc/disc.page.ts
+++ discGolf/src/app/pages/disc/disc.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-disc',
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./disc.page.scss'],
 })
 export class DiscPage implements OnInit {
+  id: string;
 
-  constructor() { }
+  constructor(private router: Router, private db: AngularFirestore) { }
 
   ngOnInit() {
-  }
+    this.id = this.router.url.slice(6);
+    this.db.collection('discs').doc(this.id).ref.get().then(function (doc) {
+      if (doc.exists) {
+        console.log(doc.data());
+      } else {
+        console.log('no doc');
+      }
+    }).catch(function (error) {
+      console.log(error);
+    });
 
+
+  }
 }
