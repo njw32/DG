5a30512048eab40bd29f378bbe21224938853602
njw32 <njw32@students.calvin.edu>
2020-12-24 16:56:14 -0600

    write service

--- discGolf/src/app/services/auth.service.ts
+++ discGolf/src/app/services/auth.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
-
+import firebase from 'firebase/app';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
   constructor() { }
+
+  loginUser(email: string, password: string): Promise<firebase.auth.UserCredential> {
+    return firebase.auth().signInWithEmailAndPassword(email, password);
+  }
+  signupUser(email: string, password: string): Promise<any> {
+    return firebase.auth().createUserWithEmailAndPassword(email, password);
+  }
+  resetPassword(email: string): Promise<void> {
+    return firebase.auth().sendPasswordResetEmail(email);
+  }
+  logoutUser(): Promise<void> { return firebase.auth().signOut(); }
+  getCurrentUser(): string { return firebase.auth().currentUser.email; }
+
 }
