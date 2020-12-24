559affbb6f69f44591abc83c0a7d5601f21c45fb
njw32 <njw32@students.calvin.edu>
2020-12-24 17:46:26 -0600

    create account functional

--- discGolf/src/app/pages/login/login.page.ts
+++ discGolf/src/app/pages/login/login.page.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit {
 
-  constructor() { }
+export class LoginPage implements OnInit {
+  email: string;
+  password: string;
+  constructor(private authSvc: AuthService, private router: Router) {
+  }
 
   ngOnInit() {
+  }
+
+  async onSubmit() {
+    try {
+      await this.authSvc.loginUser(this.email, this.password)
+      this.router.navigateByUrl('home')
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async createAccount(): Promise<void> {
+    try {
+      await this.authSvc.signupUser(this.email, this.password);
+      this.router.navigateByUrl('home')
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  resetPassword(): void {
+    if (!this.email) {
+      console.log('ALERT')
+    }
+    this.authSvc.resetPassword(this.email);
   }
 
 }
