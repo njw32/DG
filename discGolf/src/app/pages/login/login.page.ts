496201b688a3c1637023aad645deec6df37c7ca7
njw32 <njw32@students.calvin.edu>
2020-12-31 13:49:33 -0600

    alerts create account and login fails

--- discGolf/src/app/pages/login/login.page.ts
+++ discGolf/src/app/pages/login/login.page.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
-
+import { LoadingController } from '@ionic/angular';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -12,7 +13,7 @@ import { AuthService } from '../../services/auth.service';
 export class LoginPage implements OnInit {
   email: string;
   password: string;
-  constructor(private authSvc: AuthService, private router: Router) {
+  constructor(private alertCtrl: AlertController, private authSvc: AuthService, private router: Router, public loadingCrtl: LoadingController) {
   }
 
   ngOnInit() {
@@ -21,18 +22,63 @@ export class LoginPage implements OnInit {
   async onSubmit() {
     try {
       await this.authSvc.loginUser(this.email, this.password)
-      this.router.navigateByUrl('home')
+      this.router.navigateByUrl('home');
     } catch (error) {
       console.log(error);
+      const alert = await this.alertCtrl.create({
+        message: error,
+        buttons: [{
+          text: 'Ok',
+          role: 'cancel'
+        }],
+      });
+      alert.present();
     }
   }
 
-  async createAccount(): Promise<void> {
+  async createAccountPop() {
+    const alert = await this.alertCtrl.create({
+      header: 'CREATE NEW ACCOUNT',
+      inputs: [
+        {
+          name: 'email',
+          placeholder: 'sample@mail.com'
+        },
+        {
+          name: 'password',
+          placeholder: 'password',
+          type: 'password'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+        },
+        {
+          text: 'Create Account',
+          handler: data => {
+            this.createAccount(data.email, data.password)
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
+
+  async createAccount(email: string, password: string): Promise<void> {
     try {
-      await this.authSvc.signupUser(this.email, this.password);
+      await this.authSvc.signupUser(email, password);
       this.router.navigateByUrl('home')
     } catch (error) {
-      console.log(error);
+      const alert = await this.alertCtrl.create({
+        message: error,
+        buttons: [{
+          text: 'Ok',
+          role: 'cancel'
+        }],
+      });
+      alert.present();
     }
   }
 
